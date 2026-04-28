2615f26e73246d57a6b10546bad2a62d859bf464
ST10460086 <166159899+Reece52SRT@users.noreply.github.com>
2026-04-28 20:34:39 +0200

    Added Imports and declared private variables for session tracking to the CategoryViewModel.

--- app/src/main/java/com/example/kachingaapp_prog7313poe/viewmodel/CategoryViewModel.kt
+++ app/src/main/java/com/example/kachingaapp_prog7313poe/viewmodel/CategoryViewModel.kt
@@ -1,4 +1,30 @@
 package com.example.kachingaapp_prog7313poe.viewmodel
 
-class CategoryViewModel {
+
+import com.example.kachingaapp_prog7313poe.data.KachingaDatabase
+import com.example.kachingaapp_prog7313poe.data.SessionManager
+import com.example.kachingaapp_prog7313poe.data.entity.Category
+import com.example.kachingaapp_prog7313poe.data.repository.CategoryRepository
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+
+class CategoryViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repo = CategoryRepository(
+        KachingaDatabase.getDatabase(application).categoryDao()
+    )
+    private val session = SessionManager(application)
+
+    private val userId: StateFlow<Int> = session.userId
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), -1)
+
+
+
+
+
 }
