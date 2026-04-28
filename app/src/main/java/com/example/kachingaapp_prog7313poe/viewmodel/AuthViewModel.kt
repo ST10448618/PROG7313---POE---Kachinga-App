86912d902cc4f04bf3f140ed506a720b57dd36b1
ST10460086 <166159899+Reece52SRT@users.noreply.github.com>
2026-04-28 20:04:19 +0200

    Added Imports to AuthViewModel and declared session/state variables

--- app/src/main/java/com/example/kachingaapp_prog7313poe/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/kachingaapp_prog7313poe/viewmodel/AuthViewModel.kt
@@ -1,4 +1,32 @@
 package com.example.kachingaapp_prog7313poe.viewmodel
 
-class AuthViewModel {
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kachingaapp_prog7313poe.data.KachingaDatabase
+import com.example.kachingaapp_prog7313poe.data.SessionManager
+import com.example.kachingaapp_prog7313poe.data.entity.User
+import com.example.kachingaapp_prog7313poe.data.repository.UserRepository
+
+class AuthViewModel(application: Application) : AndroidViewModel(application) {
+
+     private val repo = UserRepository(
+        KachingaDatabase.getDatabase(application).userDao()
+    )
+    private val session = SessionManager(application)
+
+    val isLoggedIn: StateFlow<Boolean> = session.isLoggedIn
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), false)
+
+    private val _uiState = MutableStateFlow(AuthUiState())
+    val uiState: StateFlow<AuthUiState> = _uiState.asStateFlow()
+
+    // Separate state just for navigation — avoids calling lambda from bg thread
+    private val _navigateToHome = MutableStateFlow(false)
+    val navigateToHome: StateFlow<Boolean> = _navigateToHome.asStateFlow()
+
+    private val _navigateAfterRegister = MutableStateFlow(false)
+    val navigateAfterRegister: StateFlow<Boolean> = _navigateAfterRegister.asStateFlow()
+
 }
