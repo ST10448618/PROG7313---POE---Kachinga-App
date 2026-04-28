6d6172587e7009ffb88406ca2bb6b35b85a880d1
ST10448618 <ST10448618@myemeris.edu.za>
2026-04-28 20:05:39 +0200

    Added functionality to image utility to be used in future views

--- app/src/main/java/com/example/kachingaapp_prog7313poe/util/ImagePickerUtil.kt
+++ app/src/main/java/com/example/kachingaapp_prog7313poe/util/ImagePickerUtil.kt
@@ -1,4 +1,28 @@
-package com.example.kachingaapp_prog7313poe.util
+package com.example.prog7313_poe_kachinga.util
 
-class ImagePickerUtil {
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+object ImagePickerUtil {
+
+    fun createImageFile(context: Context): File {
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault())
+            .format(Date())
+        val storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("RECEIPT_${timestamp}_", ".jpg", storageDir)
+    }
+
+    fun getUriForFile(context: Context, file: File): Uri {
+        return FileProvider.getUriForFile(
+            context,
+            "${context.packageName}.provider",
+            file
+        )
+    }
 }
