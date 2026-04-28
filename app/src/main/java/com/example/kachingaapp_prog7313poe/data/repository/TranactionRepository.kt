57e97181381bf29e6556d99e814b47dc16d731bd
ST10441678 <ST10441678@vcconnect.edu.za>
2026-04-28 20:10:58 +0200

    The Transaction Repository Class has been updated, more functions have been added for insert, delete and update.

--- app/src/main/java/com/example/kachingaapp_prog7313poe/data/repository/TranactionRepository.kt
+++ app/src/main/java/com/example/kachingaapp_prog7313poe/data/repository/TranactionRepository.kt
@@ -3,6 +3,7 @@ package com.example.kachingaapp_prog7313poe.data.repository
 import com.example.kachingaapp_prog7313poe.data.dao.TransactionDao
 import com.example.kachingaapp_prog7313poe.data.entity.AppTransaction
 import kotlinx.coroutines.flow.Flow
+import kotlin.compareTo
 
 
 class TransactionRepository(private val dao: TransactionDao) {
@@ -23,4 +24,20 @@ class TransactionRepository(private val dao: TransactionDao) {
     fun getExpensesByDateRange(userId: Int, start: Long, end: Long): Flow<List<AppTransaction>> =
         dao.getExpensesByDateRange(userId, start, end)
 
+    suspend fun insert(transaction: AppTransaction): Result<Unit> {
+        return try {
+            if (transaction.title.isBlank())
+                return Result.failure(Exception("Title cannot be empty"))
+            if (transaction.amount <= 0)
+                return Result.failure(Exception("Amount must be greater than 0"))
+            dao.insertTransaction(transaction)
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun delete(transaction: AppTransaction) = dao.deleteTransaction(transaction)
+
+    suspend fun update(transaction: AppTransaction) = dao.updateTransaction(transaction)
 }
