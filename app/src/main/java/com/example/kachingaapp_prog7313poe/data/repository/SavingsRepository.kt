fd2565e36151c0961d54e0452808d09831ddce19
ST10441678 <ST10441678@vcconnect.edu.za>
2026-04-28 20:02:14 +0200

    The Savings Repository Class has been updated with all necessary functions added.

---
+++ app/src/main/java/com/example/kachingaapp_prog7313poe/data/repository/SavingsRepository.kt
@@ -0,0 +1,26 @@
+package com.example.kachingaapp_prog7313poe.data.repository
+
+import com.example.kachingaapp_prog7313poe.data.dao.SavingsGoalDao
+import com.example.kachingaapp_prog7313poe.data.entity.SavingsGoal
+import kotlinx.coroutines.flow.Flow
+import kotlin.compareTo
+
+class SavingsRepository(private val dao: SavingsGoalDao) {
+
+    fun getAll(userId: Int): Flow<List<SavingsGoal>> = dao.getAllGoals(userId)
+
+    fun getById(id: Int, userId: Int): Flow<SavingsGoal?> = dao.getGoalById(id, userId)
+
+    suspend fun insert(goal: SavingsGoal): Result<Unit> {
+        return try {
+            if (goal.name.isBlank())
+                return Result.failure(Exception("Goal name cannot be empty"))
+            if (goal.targetAmount <= 0)
+                return Result.failure(Exception("Target must be greater than 0"))
+            dao.insertGoal(goal)
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
