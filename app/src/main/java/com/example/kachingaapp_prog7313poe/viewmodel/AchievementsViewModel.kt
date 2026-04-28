393ce11ab687f3fb383d9362469dbcf0f796aed6
ST10460086 <166159899+Reece52SRT@users.noreply.github.com>
2026-04-28 21:01:27 +0200

    Added imports and private declarations to the AchievementsViewModel.

--- app/src/main/java/com/example/kachingaapp_prog7313poe/viewmodel/AchievementsViewModel.kt
+++ app/src/main/java/com/example/kachingaapp_prog7313poe/viewmodel/AchievementsViewModel.kt
@@ -1,4 +1,21 @@
 package com.example.kachingaapp_prog7313poe.viewmodel
 
-class AchievementsViewModel {
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kachingaapp_prog7313poe.data.KachingaDatabase
+import com.example.kachingaapp_prog7313poe.data.SessionManager
+import com.example.kachingaapp_prog7313poe.data.entity.Achievement
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class AchievementsViewModel(application: Application) : AndroidViewModel(application) {
+    private val dao = KachingaDatabase.getDatabase(application).achievementDao()
+    private val transactionDao = KachingaDatabase.getDatabase(application).transactionDao()
+    private val savingsDao = KachingaDatabase.getDatabase(application).savingsGoalDao()
+    private val categoryDao = KachingaDatabase.getDatabase(application).categoryDao()
+    private val session = SessionManager(application)
+
 }
