c43fc154683572e4415caa8e14827edf339d7d8d
ST10441678 <ST10441678@vcconnect.edu.za>
2026-06-03 16:43:23 +0200

    BillDao File has been updated with Insert, Delete and Update Functions.

--- app/src/main/java/com/example/kachingaapp_prog7313poe/data/dao/BillDao.java
+++ app/src/main/java/com/example/kachingaapp_prog7313poe/data/dao/BillDao.java
@@ -1,8 +1,18 @@
 package com.example.kachingaapp_prog7313poe.data.dao;
 
-import androidx.room.*
-import com.example.kachingaapp_prog7313poe.data.entity.Bill
-import kotlinx.coroutines.flow.Flow
+import androidx.room.*;
+import com.example.kachingaapp_prog7313poe.data.entity.Bill;
+import kotlinx.coroutines.flow.Flow;
 
-public interface BillDao {
+@Dao
+interface BillDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBill(bill: Bill)
+
+    @Delete
+    suspend fun deleteBill(bill: Bill)
+
+    @Update
+    suspend fun updateBill(bill: Bill)
 }
