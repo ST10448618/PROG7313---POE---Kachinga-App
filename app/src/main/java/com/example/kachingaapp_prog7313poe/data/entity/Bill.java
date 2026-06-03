0dfbba3d950bf88f0f0c2a435f3baee1636ef15a
ST10441678 <ST10441678@vcconnect.edu.za>
2026-06-03 16:53:22 +0200

    The Bill File has been added in the entity folder to accomodate for the Bills feature being added. Imports have been added, and the values have been added.

--- app/src/main/java/com/example/kachingaapp_prog7313poe/data/entity/Bill.java
+++ app/src/main/java/com/example/kachingaapp_prog7313poe/data/entity/Bill.java
@@ -3,5 +3,11 @@ package com.example.kachingaapp_prog7313poe.data.entity;
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-public class Bill {
-}
+@Entity(tableName = "bills")
+data class Bill(
+        @PrimaryKey(autoGenerate = true)
+        val id: Int = 0,
+        val userId: Int = 0,
+        val name: String,
+        val amount: Double
+)
