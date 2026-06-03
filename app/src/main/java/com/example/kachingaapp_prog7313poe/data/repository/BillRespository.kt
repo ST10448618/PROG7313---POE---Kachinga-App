c8774c65ded5df704ec31a1bbd75e1681b971494
ST10441678 <ST10441678@vcconnect.edu.za>
2026-06-03 17:05:47 +0200

    BillRepository File has been updated with all necessary  functions.

--- app/src/main/java/com/example/kachingaapp_prog7313poe/data/repository/BillRespository.kt
+++ app/src/main/java/com/example/kachingaapp_prog7313poe/data/repository/BillRespository.kt
@@ -3,6 +3,23 @@ package com.example.kachingaapp_prog7313poe.data.repository
 import com.example.kachingaapp_prog7313poe.data.dao.BillDao
 import com.example.kachingaapp_prog7313poe.data.entity.Bill
 import kotlinx.coroutines.flow.Flow
+import kotlin.compareTo
 
-class BillRespository {
-}
+class BillRepository(private val dao: BillDao) {
+
+    fun getAllBills(userId: Int): Flow<List<Bill>> = dao.getAllBills(userId)
+
+    suspend fun insert(bill: Bill): Result<Unit> {
+        return try {
+            if (bill.name.isBlank())
+                return Result.failure(Exception("Bill name cannot be empty"))
+            if (bill.amount <= 0)
+                return Result.failure(Exception("Amount must be greater than 0"))
+            if (bill.dueDay !in 1..31)
+                return Result.failure(Exception("Due day must be between 1 and 31"))
+            dao.insertBill(bill)
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
