b7dc3276cc475e876237fa2844d9d8f4f4f3d553
ST10441678 <ST10441678@vcconnect.edu.za>
2026-06-03 16:40:37 +0200

    BillDao File has been added to the Data's Dao Folder. Imports have been added as well.

---
+++ app/src/main/java/com/example/kachingaapp_prog7313poe/data/dao/BillDao.java
@@ -0,0 +1,8 @@
+package com.example.kachingaapp_prog7313poe.data.dao;
+
+import androidx.room.*
+import com.example.kachingaapp_prog7313poe.data.entity.Bill
+import kotlinx.coroutines.flow.Flow
+
+public interface BillDao {
+}
