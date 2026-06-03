d1c91c81534029a28b7d2bef18866c03f816dd1d
ST10441678 <ST10441678@vcconnect.edu.za>
2026-06-03 17:03:43 +0200

    BillRepository File has been added to the Repository Folder, Imports have also been added.

---
+++ app/src/main/java/com/example/kachingaapp_prog7313poe/data/repository/BillRespository.kt
@@ -0,0 +1,8 @@
+package com.example.kachingaapp_prog7313poe.data.repository
+
+import com.example.kachingaapp_prog7313poe.data.dao.BillDao
+import com.example.kachingaapp_prog7313poe.data.entity.Bill
+import kotlinx.coroutines.flow.Flow
+
+class BillRespository {
+}
